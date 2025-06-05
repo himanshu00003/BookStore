4f81963a8fb5086bbe412a3e1a08d5b7ad4dc72c
himanshu00003 <himanshumehta.283@gmail.com>
2025-06-05 19:47:00 +0530

    Added Footer Component and fixed NavBar Component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import Banner from "./Components/Banner";
+import Footer from "./Components/Footer";
+
 function App() {
   return (
     <>
       <Navbar />
       <Banner />
+      <Footer />
     </>
   );
 }
--- Frontend/src/Components/Banner.jsx
+++ Frontend/src/Components/Banner.jsx
@@ -5,7 +5,7 @@ import bbbkkks from "../../public/bbbkkks.jpg"
 function Banner() {
   return (
     <>
-      <div className="max-w-screen 2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row">
+      <div className="max-w-screen 2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row my-20 ">
         <div className="w-full order-2 md:order-1 md:w-1/2 mt:12 md:mt-32 ">
           <div className="space-y-12">
             <h1 className="text-4xl font-bold">
